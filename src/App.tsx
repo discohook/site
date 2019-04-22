2f847daee91660a529418ff4fe3207974e4094f2
vivi <me@vivi.fyi>
2019-04-22 18:34:52 +0200

    Add whitney font and global style

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { GlobalStyle } from "./GlobalStyle"
+import { Whitney } from "./Whitney"
+
+export const App = () => (
+  <div>
+    <Whitney />
+    <GlobalStyle />
+    <div>Hello, world!</div>
+  </div>
+)
---
+++ src/Whitney.tsx
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components"
+
+export const Whitney = createGlobalStyle`
+  @font-face {
+    font-family: Whitney;
+    font-weight: 300;
+    src: url("https://discordapp.com/assets/16801bfdf51ed88e69bc90626a3ea436.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: Whitney;
+    font-weight: 400;
+    src: url("https://discordapp.com/assets/d153359b5d87601d2b9c708b7ae2db02.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: Whitney;
+    font-weight: 500;
+    src: url("https://discordapp.com/assets/a934ab008c7f6a2274ec441f6be0696a.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: Whitney;
+    font-weight: 600;
+    src: url("https://discordapp.com/assets/877ef48bf9fcfee590f0ba64de101b59.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: Whitney;
+    font-weight: 700;
+    src: url("https://discordapp.com/assets/2e004dd717cdc58b9ccb361d286ebbce.woff") format("woff");
+  }
+`
