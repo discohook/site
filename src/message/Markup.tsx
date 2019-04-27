2691a96402164bb54b1cf17c314af5dbbf208813
vivi <me@vivi.fyi>
2019-04-27 18:24:43 +0200

    Do the markup with simple-markdown

--- src/message/Markup.tsx
+++ src/message/Markup.tsx
@@ -1,7 +1,14 @@
 import React from "react"
+import SimpleMarkdown from "simple-markdown"
 
 interface Props {
   content: string
 }
 
-export const Markup = (props: Props) => <div>{props.content}</div>
+export const Markup = (props: Props) => (
+  <div>
+    {SimpleMarkdown.defaultReactOutput(
+      SimpleMarkdown.defaultImplicitParse(props.content),
+    )}
+  </div>
+)
