9021fde1822b2823a36e30b6b1a093f43df859cf
vivi <me@vivi.fyi>
2019-04-29 17:53:26 +0200

    Change font stack

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -2,10 +2,14 @@ import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    font-family: "Whitney", sans-serif;
+    font-family: "Whitney", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
     line-height: 1;
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: "Consolas", "Liberation Mono", "Menlo", "Courier", monospace;
   }
 `
