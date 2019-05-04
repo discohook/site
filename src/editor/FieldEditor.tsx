cb42e31cd8a4f444a0c9f108cbb83ad39c83250a
vivi <me@vivi.fyi>
2019-05-04 14:16:44 +0200

    Add inline toggle on fields

--- src/editor/FieldEditor.tsx
+++ src/editor/FieldEditor.tsx
@@ -19,13 +19,60 @@ const Container = styled.div`
   border-radius: 3px;
 `
 
+const InlineButton = styled.button<{ inline: boolean }>`
+  width: 80px;
+  margin: 8px 8px 8px 4px;
+  padding: 0 16px;
+  height: 40px;
+
+  background: ${(props) => (props.inline ? "#7289da" : "transparent")};
+  border: 1px solid #7289da;
+  border-radius: 3px;
+  outline: none;
+  cursor: pointer;
+
+  color: #ffffff;
+  font-family: "Whitney", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+  font-size: 15px;
+  line-height: 20px;
+  letter-spacing: -0.4px;
+
+  transition: 300ms;
+
+  :hover {
+    opacity: 0.8;
+  }
+`
+
+const HorizontalContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+
+  > *:not(${InlineButton}) {
+    flex: 1;
+  }
+`
+
 export const FieldEditor = (props: Props) => (
   <Container>
-    <InputField
-      value={props.field.name || ""}
-      onChange={(name) => props.onChange({ ...props.field, name })}
-      label="Field name"
-    />
+    <HorizontalContainer>
+      <InputField
+        value={props.field.name || ""}
+        onChange={(name) => props.onChange({ ...props.field, name })}
+        label="Field name"
+      />
+      <InlineButton
+        inline={props.field.inline || false}
+        onClick={() =>
+          props.onChange({
+            ...props.field,
+            inline: !props.field.inline || undefined,
+          })
+        }
+      >
+        Inline
+      </InlineButton>
+    </HorizontalContainer>
     <InputField
       value={props.field.value || ""}
       onChange={(value) => props.onChange({ ...props.field, value })}
