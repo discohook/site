07c707677ba297858d1037a24180d9f5cbb8334f
vivi <me@vivi.fyi>
2019-05-28 11:46:17 +0200

    Setup webpack + babel

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const { resolve } = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin").default
+
+const dev = process.env.NODE_ENV === "development"
+
+/** @type {import("webpack").Configuration} */
+module.exports = {
+  mode: dev ? "development" : "production",
+  entry: resolve(__dirname, "src", "index.tsx"),
+  output: {
+    filename: "main.[hash].js",
+    path: resolve(__dirname, "dist"),
+  },
+  devtool: dev ? "source-map" : false,
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      { test: /\.[jt]sx?$/, use: "babel-loader" },
+      { enforce: "pre", test: /\.js$/, use: "source-map-loader" },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: resolve(__dirname, "dist", "index.html"),
+      template: resolve(__dirname, "public", "index.html"),
+      minify: !dev && {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeAttributeQuotes: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    host: "localhost",
+    port: 3000,
+    hot: true,
+  },
+}
