9b0f40ed6870f70b8cc95dfa7e9f3da69474e3a9
vivi <me@vivi.fyi>
2019-05-29 22:52:44 +0200

    Changed up dynamic highlight.js importing logic

--- src/message/markup/highlight.ts
+++ src/message/markup/highlight.ts
@@ -1,27 +1,29 @@
-import languages from "./languages.json"
-
 const hljs: typeof import("highlight.js") = require("highlight.js/lib/highlight")
+const languages: Record<string, Language> = require("./languages.json")
 
-const aliasToName: Record<string, keyof typeof languages> = {}
+type Language = string[] | { aliases?: string[]; dependencies?: string[] }
+
+const aliasToName: Record<string, string> = {}
 for (const [name, language] of Object.entries(languages)) {
-  for (const alias of language.aliases) {
-    aliasToName[alias] = name as keyof typeof languages
-  }
+  aliasToName[name] = name
+
+  const aliases = Array.isArray(language) ? language : language.aliases || []
+  for (const alias of aliases) aliasToName[alias] = name
 }
 
 const importLanguage = async (language: string) => {
   const lang = languages[aliasToName[language]]
 
-  if (lang.dependencies)
+  if (!Array.isArray(lang) && lang.dependencies)
     await Promise.all(lang.dependencies.map(importLanguage))
 
   const { default: importedLanguage } = await import(
-    "highlight.js/lib/languages/" + aliasToName[language]
+    `highlight.js/lib/languages/${aliasToName[language]}.js`
   )
 
   hljs.registerLanguage(language, importedLanguage)
 
-  console.log("Registered highlight.js language:", language)
+  console.log("Registered highlight.js language:", aliasToName[language])
 }
 
 export const highlight = async (language: string, content: string) => {
