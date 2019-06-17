af58166a8f47f35d877c5eca031b0d29d68231d3
vivi <me@vivi.fyi>
2019-06-17 20:40:13 +0200

    Write my own idb-keyval for transpilation

--- src/editor/backup/backupStorage.ts
+++ src/editor/backup/backupStorage.ts
@@ -1,4 +1,3 @@
-import { del, get, keys, set, Store } from "idb-keyval"
 import { Message } from "../../message/Message"
 
 export interface Backup {
@@ -6,20 +5,68 @@ export interface Backup {
   files: string[]
 }
 
-const backupStore = new Store("backups", "backupStore")
+const dbPromise = new Promise<IDBDatabase>((res, rej) => {
+  const openRequest = indexedDB.open("backups", 1)
+
+  openRequest.addEventListener("success", () => res(openRequest.result))
+  openRequest.addEventListener("error", () => rej(openRequest.error))
+
+  openRequest.addEventListener("upgradeneeded", () =>
+    openRequest.result.createObjectStore("backupStore"),
+  )
+})
+
+let db!: IDBDatabase
+dbPromise.then((database) => (db = database))
+
+const runTransaction = async (
+  mode: IDBTransactionMode,
+  callback: (store: IDBObjectStore) => void,
+) => {
+  await dbPromise
+
+  return new Promise((res, rej) => {
+    const transaction = db.transaction("backupStore", mode)
+
+    transaction.addEventListener("complete", () => res())
+    transaction.addEventListener("error", () => rej(transaction.error))
+    transaction.addEventListener("abort", () => rej(transaction.error))
+
+    callback(transaction.objectStore("backupStore"))
+  })
+}
 
 export const getBackups = async () => {
-  return (await keys(backupStore)) as string[]
+  const keys: IDBValidKey[] = []
+
+  await runTransaction("readonly", (store) => {
+    const cursor = store.openKeyCursor || store.openCursor
+    const request = cursor()
+
+    request.addEventListener("success", () => {
+      if (!request.result) return
+      keys.push(request.result!.key)
+      request.result.continue()
+    })
+
+    request.addEventListener("error", () => {
+      throw request.error
+    })
+  })
+
+  return keys as string[]
 }
 
 export const setBackup = async (name: string, backup: Backup) => {
-  await set(name, backup, backupStore)
+  await runTransaction("readwrite", (store) => store.put(backup, name))
 }
 
 export const getBackup = async (name: string) => {
-  return (await get(name, backupStore)) as Backup | undefined
+  let request: IDBRequest
+  await runTransaction("readonly", (store) => (request = store.get(name)))
+  return request!.result
 }
 
 export const deleteBackup = async (name: string) => {
-  await del(name, backupStore)
+  await runTransaction("readwrite", (store) => store.delete(name))
 }
