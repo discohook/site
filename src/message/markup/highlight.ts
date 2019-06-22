b31b6bc45e64b96c862136ebee82409e5f7a4a06
vivi <me@vivi.fyi>
2019-06-23 01:03:11 +0200

    Attempt to clean up highlight.js import logic

--- src/message/markup/highlight.ts
+++ src/message/markup/highlight.ts
@@ -1,4 +1,5 @@
 const hljs: typeof import("highlight.js") = require("highlight.js/lib/highlight")
+import { HLJSStatic, IModeBase } from "highlight.js"
 import { languages } from "./languages"
 
 const aliases: Record<string, string> = {}
@@ -8,22 +9,24 @@ for (const language of languages) {
   for (const alias of language.aliases) aliases[alias] = name
 }
 
+type HLJSLanguage = (hljs?: HLJSStatic) => IModeBase
+
 const importLanguage = async (name: string) => {
   const lang = languages.find((lang) => lang.aliases.includes(name))
   if (!lang) return
 
   const languagesToImport = [...(lang.dependencies || []), aliases[name]]
-  const hljsLanguages = await Promise.all(
-    languagesToImport.map((name) =>
-      import(
-        /* webpackChunkName: "hljs-[request]" */
-        `highlight.js/lib/languages/${name}`
-      ),
-    ),
+  const hljsLanguages = languagesToImport.map(
+    async (name) =>
+      [
+        name,
+        (await import(
+          `highlight.js/lib/languages/${name}` /* webpackChunkName: "hljs-[request]" */
+        )).default,
+      ] as [string, HLJSLanguage],
   )
 
-  for (const [index, { default: language }] of Object.entries(hljsLanguages)) {
-    const name = languagesToImport[Number(index)]
+  for await (const [name, language] of hljsLanguages) {
     hljs.registerLanguage(name, language)
     console.log("Registered highlight.js language:", name)
   }
