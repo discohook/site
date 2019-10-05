4c8e3eb96cdf82dfbc8c680ba15988b21bc7be77
vivi <me@vivi.fyi>
2019-10-05 12:37:57 +0000

    Simplify applyIds

--- src/editor/json/applyIds.ts
+++ src/editor/json/applyIds.ts
@@ -4,23 +4,25 @@ import { getUniqueId, id } from "../../uid"
 export const applyIds = (message: Message) => {
   const newMessage = { ...message }
 
-  if (Array.isArray(newMessage.embeds)) {
-    for (const index of newMessage.embeds.keys()) {
-      const embed = {
-        ...newMessage.embeds[index],
+  if (!Array.isArray(newMessage.embeds)) return newMessage
+
+  for (const index of newMessage.embeds.keys()) {
+    const embed = {
+      ...newMessage.embeds[index],
+      [id]: getUniqueId(),
+    }
+
+    newMessage.embeds[index] = embed
+
+    if (!Array.isArray(embed.fields)) continue
+
+    for (const index of embed.fields.keys()) {
+      const field = {
+        ...embed.fields[index],
         [id]: getUniqueId(),
       }
 
-      newMessage.embeds[index] = embed
-
-      if (Array.isArray(embed.fields)) {
-        for (const index of embed.fields.keys()) {
-          embed.fields[index] = {
-            ...embed.fields[index],
-            [id]: getUniqueId(),
-          }
-        }
-      }
+      embed.fields[index] = field
     }
   }
 
