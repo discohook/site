9bd13645207beaffd26f6470838f1a5d49b14749
vivi <me@vivi.fyi>
2019-10-09 19:58:53 +0000

    Give request context a display name

--- src/RequestContext.tsx
+++ src/RequestContext.tsx
@@ -2,6 +2,7 @@ import { Context } from "koa"
 import { createContext } from "react"
 
 export const RequestContext = createContext<Partial<Context>>({})
+RequestContext.displayName = "RequestContext"
 
 export const RequestProvider = RequestContext.Provider
 export const RequestConsumer = RequestContext.Consumer
