1f9b44f88f9a7804a5cf29fd6341d6204cd2ee74
vivi <me@vivi.fyi>
2019-10-25 16:08:25 +0000

    Fix error regex in convert to be non-greedy

--- src/json/convert.ts
+++ src/json/convert.ts
@@ -16,7 +16,7 @@ export const parseMessage = (json: string) => {
   const camelCase = toCamelCase(parsedJson)
 
   const errors = isMessage(camelCase, "$").map(error => {
-    const [, key, message] = /(.*): (.*)/.exec(error)
+    const [, key, message] = /(.*?): (.*)/.exec(error)
     const snakeCaseKey = key.replace(/[A-Z]/g, m => `_${m.toLowerCase()}`)
     return `${snakeCaseKey}: ${message}`
   })
