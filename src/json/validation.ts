cff11eb84d7f7ed703a17b0038c696000d49104a
vivi <me@vivi.fyi>
2019-11-03 00:34:36 +0000

    Remove useless type cast

--- src/json/validation.ts
+++ src/json/validation.ts
@@ -54,10 +54,7 @@ export const contains = (validate: Validator): Validator =>
 export const isShape = (shape: Record<string, Validator>): Validator =>
   first(isObject, (value, key) =>
     Object.entries(shape).flatMap(([shapeKey, validate]) =>
-      Object.prototype.hasOwnProperty.call(
-        value as Record<string, unknown>,
-        shapeKey,
-      )
+      Object.prototype.hasOwnProperty.call(value, shapeKey)
         ? validate(
             (value as Record<string, unknown>)[shapeKey],
             `${key}.${shapeKey}`,
