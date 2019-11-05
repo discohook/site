a1a761d3f05aabf170f55c0a26cdb1c22e2ac9e9
vivi <me@vivi.fyi>
2019-11-05 18:37:44 +0000

    Sort imports

--- src/preview/EmbedField.tsx
+++ src/preview/EmbedField.tsx
@@ -1,5 +1,5 @@
-import styled from "@emotion/styled"
 import { css } from "@emotion/core"
+import styled from "@emotion/styled"
 import React from "react"
 import { Theme } from "../core/themes"
 import Markup from "../markup/Markup"
