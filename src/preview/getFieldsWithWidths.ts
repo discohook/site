d3d597434f4043deff5622717c2b480308e93960
vivi <me@vivi.fyi>
2019-11-22 21:10:45 +0000

    Use immer on getFieldsWithWidths

--- src/preview/getFieldsWithWidths.ts
+++ src/preview/getFieldsWithWidths.ts
@@ -1,44 +1,43 @@
+import { Draft, produce } from "immer"
 import { Field } from "../message/Message"
 
-type FieldWithWidth = Field & { width: string }
+type FieldWithWidth = Field & { readonly width: string }
 
-export const getFieldsWithWidths = (fields: readonly Field[]) => {
-  const fieldsWithWidths: FieldWithWidth[] = fields.map(field => ({
-    ...field,
-    width: "1 / 13",
-  }))
+export const getFieldsWithWidths = (
+  fields: readonly Field[],
+): readonly FieldWithWidth[] =>
+  produce(fields, (fields: Draft<FieldWithWidth[]>) => {
+    let inlineFieldCount = 0
 
-  let inlineFieldCount = 0
-
-  for (const [index, field] of fields.entries()) {
-    if (!field.inline) {
-      inlineFieldCount = 0
-      continue
-    }
-
-    inlineFieldCount += 1
-
-    switch (inlineFieldCount) {
-      case 1: {
-        break
-      }
-      case 2: {
-        fieldsWithWidths[index - 1].width = "1 / 7"
-        fieldsWithWidths[index].width = "7 / 13"
-
-        break
-      }
-      case 3: {
-        fieldsWithWidths[index - 2].width = "1 / 5"
-        fieldsWithWidths[index - 1].width = "5 / 9"
-        fieldsWithWidths[index].width = "9 / 13"
-
+    for (const [index, field] of fields.entries()) {
+      if (!field.inline) {
+        field.width = "1 / 13"
         inlineFieldCount = 0
+        continue
+      }
 
-        break
+      inlineFieldCount += 1
+
+      switch (inlineFieldCount) {
+        case 1: {
+          field.width = "1 / 13"
+          break
+        }
+        case 2: {
+          fields[index - 1].width = "1 / 7"
+          field.width = "7 / 13"
+          break
+        }
+        case 3: {
+          inlineFieldCount = 0
+
+          fields[index - 2].width = "1 / 5"
+          fields[index - 1].width = "5 / 9"
+          field.width = "9 / 13"
+          break
+        }
       }
     }
-  }
 
-  return fieldsWithWidths
-}
+    return fields
+  })
