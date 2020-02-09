98ca3fbe6779e35951242d074691eb70e897de9c
vivi <me@vivi.fyi>
2020-02-09 15:03:35 +0000

    Memoize Markdown component

--- src/markdown/components/Markdown.tsx
+++ src/markdown/components/Markdown.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { memo } from "react"
 import { PARSERS } from "../constants/parsers"
 import { MarkdownContainer } from "./MarkdownContainer"
 
@@ -7,10 +7,12 @@ export type MarkdownProps = {
   type: "message-content" | "embed-content" | "embed-header"
 }
 
-export function Markdown(props: MarkdownProps) {
+function MarkdownRenderer(props: MarkdownProps) {
   const { content, type } = props
 
   const parse = PARSERS[type]
 
   return <MarkdownContainer>{parse(content.trim())}</MarkdownContainer>
 }
+
+export const Markdown = memo(MarkdownRenderer)
