d73e999a7361f2f845d8fee3fa7869498ac76ef5
vivi <me@vivi.fyi>
2020-02-12 21:44:53 +0000

    Wait for document load before hydrate

--- src/core/client.tsx
+++ src/core/client.tsx
@@ -3,15 +3,17 @@ import { hydrate, render } from "react-dom"
 import { App } from "./components/App"
 import { ErrorBoundary } from "./components/ErrorBoundary"
 
-const app = (
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
-)
+addEventListener("load", () => {
+  const app = (
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  )
 
-const container = document.querySelector("#app")
-if (container?.hasChildNodes()) {
-  hydrate(app, container)
-} else {
-  render(app, container)
-}
+  const container = document.querySelector("#app")
+  if (container?.hasChildNodes()) {
+    hydrate(app, container)
+  } else {
+    render(app, container)
+  }
+})
