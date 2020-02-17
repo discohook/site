280518968c1a885bd81f16a2233c1b718d38ff20
vivi <me@vivi.fyi>
2020-02-17 20:52:01 +0000

    Remove cache headers from static files

--- src/ssr/middleware/serveStaticFiles.ts
+++ src/ssr/middleware/serveStaticFiles.ts
@@ -1,9 +1,4 @@
 import serve from "koa-static"
 import { BUILD_FOLDER } from "../constants"
 
-export const serveStaticFiles = () =>
-  serve(BUILD_FOLDER, {
-    index: false,
-    maxAge: 31536000,
-    immutable: true,
-  })
+export const serveStaticFiles = () => serve(BUILD_FOLDER, { index: false })
