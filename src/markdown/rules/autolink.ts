0670d4543ddccd89439e0f33e6632fde2c5f6d0c
vivi <me@vivi.fyi>
2020-02-22 16:41:50 +0000

    Fix markdown parser crash

--- src/markdown/rules/autolink.ts
+++ src/markdown/rules/autolink.ts
@@ -1,8 +1,10 @@
 import { defaultRules } from "simple-markdown"
 import { parseLink } from "../helpers/parseLink"
 import { MarkdownRule } from "../types/MarkdownRule"
+import { link } from "./link"
 
 export const autolink: MarkdownRule = {
   ...defaultRules.autolink,
   parse: parseLink,
+  react: link.react,
 }
