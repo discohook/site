8792f2bfb37777f7e5afb62cd34c78937811b490
vivi <me@vivi.fyi>
2020-02-28 16:06:45 +0000

    Remove safe option from base64Encode

--- src/base64/helpers/base64Encode.ts
+++ src/base64/helpers/base64Encode.ts
@@ -1,15 +1,6 @@
-export const base64Encode = (utf8: string, safe = false) => {
+export const base64Encode = (utf8: string) => {
   if (SERVER) {
-    let base64 = Buffer.from(utf8, "utf8").toString("base64")
-
-    if (safe) {
-      base64 = base64
-        .replace(/\+/g, "-")
-        .replace(/\//g, "_")
-        .replace(/=/g, "")
-    }
-
-    return base64
+    return Buffer.from(utf8, "utf8").toString("base64")
   }
 
   const encoded = encodeURIComponent(utf8)
@@ -18,14 +9,5 @@ export const base64Encode = (utf8: string, safe = false) => {
     return String.fromCharCode(parseInt(hex.slice(1), 16))
   })
 
-  let base64 = btoa(escaped)
-
-  if (safe) {
-    base64 = base64
-      .replace(/\+/g, "-")
-      .replace(/\//g, "_")
-      .replace(/=/g, "")
-  }
-
-  return base64
+  return btoa(escaped)
 }
