103706aa65478aa7e1dc97d47f2d548196f9c18c
vivi <me@vivi.fyi>
2020-05-05 01:20:02 +0100

    Make 'app' in 'discordapp.com' optional

--- src/webhook/constants.ts
+++ src/webhook/constants.ts
@@ -4,7 +4,7 @@ export const DEFAULT_AVATAR_URL =
   "https://discohook.org/assets/discord-avatar.png"
 export const DEFAULT_DISPLAY_NAME = "Discohook"
 
-export const DISCORD_HOST_RE = /(?:www\.|ptb\.|canary\.)?discordapp\.com/
+export const DISCORD_HOST_RE = /(?:www\.|ptb\.|canary\.)?discord(?:app)?\.com/
 export const WEBHOOK_ID_RE = /\d+/
 export const WEBHOOK_TOKEN_RE = /[\w-]+/
 export const WEBHOOK_URL_RE = new RegExp(
