309a309a1bd9e972c36deb2a6248cbfedb2486f3
vivi <me@vivi.fyi>
2020-05-23 23:36:16 +0100

    Add bot page

---
+++ pages/bot.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react"
+import styled from "styled-components"
+import { PageHead } from "../common/PageHead"
+
+const Container = styled.div`
+  margin: 64px 16px;
+  text-align: center;
+`
+
+const Header = styled.h1`
+  color: ${({ theme }) => theme.header.primary};
+`
+
+const BOT_INVITE =
+  "https://discord.com/api/oauth2/authorize?client_id=633565743103082527&permissions=537250880&scope=bot"
+
+export default function Discord() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.location.replace(BOT_INVITE)
+    }
+  })
+
+  return (
+    <Container>
+      <PageHead title="Discord Support Server" description="" />
+      <Header>You are being redirected</Header>
+      <p>
+        If nothing is happenning, please navigate to{" "}
+        <a href={BOT_INVITE}>{BOT_INVITE}</a>.
+      </p>
+    </Container>
+  )
+}
