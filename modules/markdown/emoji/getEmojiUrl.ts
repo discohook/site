935170c0269aee18fc9764388b5d8a131a6b6c71
vivi <me@vivi.fyi>
2020-06-22 11:30:28 +0100

    Stop removing fe0f from emoji urls

--- modules/markdown/emoji/getEmojiUrl.ts
+++ modules/markdown/emoji/getEmojiUrl.ts
@@ -3,8 +3,6 @@ const EMOJI_CDN_BASE_URL = "https://twemoji.maxcdn.com/v/12.1.4/svg"
 export const getEmojiUrl = (emoji: string) => {
   const file = [...emoji]
     .map(character => character.codePointAt(0)?.toString(16))
-    // Twemoji does not include '\uFE0F' (VARIATION SELECTOR) in their file names
-    .filter(codePoint => codePoint !== "fe0f")
     .join("-")
 
   return `${EMOJI_CDN_BASE_URL}/${file}.svg`
