4fd8f6d2d6c8796e11f8d87e5338e4cc2d23e1d2
vivi <me@vivi.fyi>
2020-06-26 19:23:23 +0100

    Fix twemoji urls again

--- modules/markdown/emoji/getEmojiUrl.ts
+++ modules/markdown/emoji/getEmojiUrl.ts
@@ -3,6 +3,9 @@ const EMOJI_CDN_BASE_URL = "https://twemoji.maxcdn.com/v/12.1.4/svg"
 export const getEmojiUrl = (emoji: string) => {
   const file = [...emoji]
     .map(character => character.codePointAt(0)?.toString(16))
+    // Twemoji does not include "\uFE0F" (VARIATION SELECTOR) in their file names
+    // *if* the emoji *does not* contain "\u200D" (ZERO WIDTH JOINER)
+    .filter((item, index, array) => array.includes("200d") || item !== "fe0f")
     .join("-")
 
   return `${EMOJI_CDN_BASE_URL}/${file}.svg`
