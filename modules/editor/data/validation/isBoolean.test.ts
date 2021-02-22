facfdd66cfc03c5ebf0742f2bc9e347325809445
vivi <me@vivi.fyi>
2021-02-22 19:44:51 +0100

    What is this

--- modules/editor/data/validation/isBoolean.test.ts
+++ modules/editor/data/validation/isBoolean.test.ts
@@ -9,7 +9,7 @@ describe("isBoolean", () => {
   it("can validate if a value is not a boolean", () => {
     expect(isBoolean("trust me it's a string", "$")).not.toHaveLength(0)
     expect(isBoolean(1234567890, "$")).not.toHaveLength(0)
-    expect(isBoolean(-(-(Number.NEGATIVE_INFINITY)), "$")).not.toHaveLength(0)
+    expect(isBoolean(Number.POSITIVE_INFINITY, "$")).not.toHaveLength(0)
     expect(isBoolean({}, "$")).not.toHaveLength(0)
     expect(isBoolean({ foo: "bar" }, "$")).not.toHaveLength(0)
     expect(isBoolean([], "$")).not.toHaveLength(0)
