37067ef18569d176b2c245db70804febe4e9b412
vivi <me@vivi.fyi>
2021-02-24 19:00:51 +0100

    Get custom build ID from env or commit hash

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/order */
+
+const cp = require("child_process")
 
 const config = {
   target: "serverless",
@@ -6,6 +9,16 @@ const config = {
   distDir: "build",
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) return process.env.BUILD_ID
+
+    return new Promise((resolve, reject) => {
+      cp.execFile("git", ["rev-parse", "--short", "HEAD"], (error, stdout) => {
+        if (error) reject(error)
+        resolve(stdout)
+      })
+    })
+  },
   redirects: async () =>
     Promise.resolve([
       {
