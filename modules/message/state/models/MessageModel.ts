49970ab3a4a0599dc831401ccb1ac8ec5c77d258
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-07-28 19:58:19 +0300

    Sync attachments on PATCH

--- modules/message/state/models/MessageModel.ts
+++ modules/message/state/models/MessageModel.ts
@@ -5,6 +5,7 @@ import { nullableDate } from "../../../../common/state/nullableDate"
 import { getUniqueId } from "../../../../common/state/uid"
 import { stringifyMessage } from "../../helpers/stringifyMessage"
 import type { MessageData } from "../data/MessageData"
+import { AttachmentModel, isLocal, isNotLocal } from "./AttachmentModel"
 import { EmbedModel } from "./EmbedModel"
 
 export const MessageModel = types
@@ -17,16 +18,20 @@ export const MessageModel = types
     reference: "",
     timestamp: types.optional(nullableDate, null),
     badge: types.optional(types.maybeNull(types.string), "Bot"),
+    attachments: types.array(types.late(() => AttachmentModel)),
   })
   .volatile(() => ({
-    files: [] as readonly File[],
   }))
   .views(self => ({
+    get filesSize() {
+      return self.attachments.filter(isLocal).reduce((acc, attachment) => acc + attachment.size, 0)
+    },
+
     get hasContent() {
       return self.content.trim().length > 0
     },
     get hasExtras() {
-      return self.embeds.length > 0 || self.files.length > 0
+      return self.embeds.length > 0 || self.attachments.length > 0
     },
 
     get embedLength() {
@@ -39,26 +44,28 @@ export const MessageModel = types
 
     get data(): MessageData {
       const embeds = self.embeds.flatMap(embed => embed.data)
+      const attachments = self.attachments.filter(isNotLocal)
 
       return {
         content: self.content || null,
         embeds: embeds.length > 0 ? embeds : null,
         username: self.username || undefined,
         avatar_url: self.avatar || undefined,
-        files: self.files.length > 0 ? Array.from(self.files) : undefined,
+        attachments: attachments.length > 0 ? attachments : [],
       }
     },
 
     get body() {
       const json = stringifyMessage(this.data, false)
+      const localFiles = self.attachments.filter(isLocal)
 
-      if (self.files.length > 0) {
+      if (localFiles.length > 0) {
         const formData = new FormData()
 
         if (json !== "{}") formData.append("payload_json", json)
 
-        for (const [index, file] of self.files.entries()) {
-          formData.append(`file[${index}]`, file, file.name)
+        for (const [index, attachment] of localFiles.entries()) {
+          formData.append(`file[${index}]`, attachment.file, attachment.filename)
         }
 
         return formData
@@ -74,7 +81,23 @@ export const MessageModel = types
     ): void {
       self[key] = value
     },
+    setFiles(files: readonly File[]) {
+      const attachments = self.attachments.filter(isNotLocal)
+      const localFiles = files.map(file => {
+        const attachment = AttachmentModel.create({
+          contentType: file.type,
+          filename: file.name,
+          local: true,
+          size: file.size,
+          url: URL.createObjectURL(file)
+        })
+        attachment.set("file", file)
+
+        return attachment
+      })
+      self.attachments.replace([...attachments, ...localFiles])
+    }
   }))
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/consistent-type-definitions
-export interface MessageLike extends Instance<typeof MessageModel> {}
+export interface MessageLike extends Instance<typeof MessageModel> { }
