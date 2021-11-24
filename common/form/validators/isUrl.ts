f005f7c0a478441772f3bb10d3f9fdaae79fb9bd
vivi <git@vivi.zone>
2021-11-24 16:02:22 +0100

    Allowing URLs starting with '%' or '{'

--- common/form/validators/isUrl.ts
+++ common/form/validators/isUrl.ts
@@ -1,3 +1,3 @@
 import { matchesRegex } from "./matchesRegex"
 
-export const isUrl = () => matchesRegex(/^https?:\/\//, "Invalid URL")
+export const isUrl = () => matchesRegex(/^(?:https?:\/\/|[%{])/, "Invalid URL")
