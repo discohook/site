9a936b0ccaff5ae8749a94136ea8f8b3a7051c8b
shay <bakkcat@gmail.com>
2022-04-09 15:29:18 +0200

    Appease vivi

--- modules/message/state/data/MessageData.ts
+++ modules/message/state/data/MessageData.ts
@@ -6,5 +6,5 @@ export type MessageData = {
   readonly username?: string
   readonly avatar_url?: string
   readonly files?: readonly File[]
-  readonly attachments?: readonly []
+  readonly attachments?: readonly unknown[]
 }
