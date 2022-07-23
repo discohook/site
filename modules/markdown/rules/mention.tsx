87997445511d0dfb0d586cdc0df452d54aa7554d
Antouto <anthonydsmart@gmail.com>
2022-07-23 16:29:30 +0200

    Format command mentions

--- modules/markdown/rules/mention.tsx
+++ modules/markdown/rules/mention.tsx
@@ -3,7 +3,7 @@ import { defaultRules, inlineRegex } from "simple-markdown"
 import type { MarkdownRule } from "../parsers/MarkdownRule"
 import { Mention } from "../styles/Mention"
 
-const MENTION_RE = /^<(@!?|@&|#)\d+>|^(@(?:everyone|here))/
+const MENTION_RE = /^<(@!?|@&|#)\d+>|^<(\/[-_ A-Za-z]{1,32}):\d+>|^(@(?:everyone|here))/
 
 const MENTION_TYPES = new Map(
   Object.entries({
@@ -18,11 +18,17 @@ export const mention: MarkdownRule = {
   order: defaultRules.text.order,
   match: inlineRegex(MENTION_RE),
   parse: capture => {
-    const [, type, everyoneOrHere] = capture
+    const [, type, command, everyoneOrHere] = capture
 
     if (everyoneOrHere) {
       return {
         content: everyoneOrHere,
+      }
+    }
+
+    if (command) {
+      return {
+        content: command,
       }
     }
 
