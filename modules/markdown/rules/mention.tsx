2da4503c00bc61d21a92c4332005f8f03de03d13
Antouto <anthonydsmart@gmail.com>
2022-07-25 12:10:53 +0200

    update regex

--- modules/markdown/rules/mention.tsx
+++ modules/markdown/rules/mention.tsx
@@ -3,7 +3,7 @@ import { defaultRules, inlineRegex } from "simple-markdown"
 import type { MarkdownRule } from "../parsers/MarkdownRule"
 import { Mention } from "../styles/Mention"
 
-const MENTION_RE = /^<(@!?|@&|#)\d+>|^<(\/[\w -]{1,32}):\d+>|^(@(?:everyone|here))/
+const MENTION_RE = /^<(@!?|@&|#)\d+>|^<(\/(?! )[\w -]+(?<! )):\d+>|^(@(?:everyone|here))/
 
 const MENTION_TYPES = new Map(
   Object.entries({
@@ -20,15 +20,15 @@ export const mention: MarkdownRule = {
   parse: capture => {
     const [, type, command, everyoneOrHere] = capture
 
-    if (everyoneOrHere) {
-      return {
-        content: everyoneOrHere,
-      }
-    }
-
     if (command) {
       return {
         content: command,
+      }
+    }
+
+    if (everyoneOrHere) {
+      return {
+        content: everyoneOrHere,
       }
     }
 
