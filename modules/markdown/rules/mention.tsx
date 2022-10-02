c42c3a0a24ec852aa0e9128b8f92893d19fb8b94
shay <bakkcat@gmail.com>
2022-10-02 16:41:15 +0200

    `span` data attributes for mention types & IDs

--- modules/markdown/rules/mention.tsx
+++ modules/markdown/rules/mention.tsx
@@ -3,9 +3,18 @@ import { defaultRules, inlineRegex } from "simple-markdown"
 import type { MarkdownRule } from "../parsers/MarkdownRule"
 import { Mention } from "../styles/Mention"
 
-const MENTION_RE = /^<(@!?|@&|#)\d+>|^<(\/(?! )[\w -]*[\w-]):\d+>|^(@(?:everyone|here))/
+const MENTION_RE = /^<(@!?|@&|#)(\d+)>|^<(\/(?! )[\w -]*[\w-]):(\d+)>|^(@(?:everyone|here))/
 
 const MENTION_TYPES = new Map(
+  Object.entries({
+    "@": "user",
+    "@!": "user",
+    "@&": "role",
+    "#": "channel",
+  }),
+)
+
+const MENTION_MATCHERS_CONTENT = new Map(
   Object.entries({
     "@": "@user",
     "@!": "@user",
@@ -18,25 +27,30 @@ export const mention: MarkdownRule = {
   order: defaultRules.text.order,
   match: inlineRegex(MENTION_RE),
   parse: capture => {
-    const [, type, command, everyoneOrHere] = capture
+    const [, matcher, matchedId, command, commandId, everyoneOrHere] = capture
 
     if (command) {
       return {
+        mentionType: 'command',
+        mentionId: commandId,
         content: command,
       }
     }
 
     if (everyoneOrHere) {
       return {
+        mentionType: 'everyone-here',
         content: everyoneOrHere,
       }
     }
 
     return {
-      content: MENTION_TYPES.get(type),
+      mentionType: MENTION_TYPES.get(matcher),
+      mentionId: matchedId,
+      content: MENTION_MATCHERS_CONTENT.get(matcher),
     }
   },
   react: (node, output, state) => (
-    <Mention key={state.key}>{node.content}</Mention>
+    <Mention key={state.key} data-mention-type={node.mentionType} data-mention-id={node.mentionId}>{node.content}</Mention>
   ),
 }
