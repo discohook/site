e98c7dd89b410e63bfadcce81c8dd13f22537e70
shay <43248357+shayypy@users.noreply.github.com>
2023-07-05 20:08:24 -0500

    Username validation warning

---
+++ common/form/validators/isUsername.ts
@@ -0,0 +1,13 @@
+export const isUsername = () => {return (value: string): string | false => {
+  for (const forbidden of ["discord", "```"]) {
+    if (value.toLowerCase().includes(forbidden)) {
+      return `Username cannot contain "${forbidden}"`;
+    }
+  }
+  for (const forbidden of ["everyone", "here"]) {
+    if (value.toLowerCase().includes(forbidden)) {
+       return `Username cannot be "${forbidden}"`;
+    }
+  }
+  return false
+}}
